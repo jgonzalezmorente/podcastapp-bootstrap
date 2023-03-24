677d75de05ffe6baaf5ce38e8fb032a5039a3f6d
José Antonio González Morente <jgonzalezmorente@gmail.com>
2023-03-24 19:14:51 +0100

    Proyecto finalizado

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 const { src, dest, watch, series } = require( 'gulp' );
 const sass = require( 'gulp-sass' )( require( 'sass' ) );
 const imagemin = require( 'gulp-imagemin' );
+const purgecss = require( 'gulp-purgecss' );
+const rename = require( 'gulp-rename' );
 
 function css( done ) {
 
@@ -23,7 +25,21 @@ function imagenes( done ) {
     done();
 }
 
+function cssbuild( done ) {
+    src( 'build/css/app.css' )
+        .pipe( rename({
+            suffix: '.min',
+        }))
+        .pipe( purgecss({
+            content: [ 'index.html' ]
+        }))
+        .pipe( dest( 'build/css' ) );
+
+    done();
+}
+
 exports.css = css;
 exports.dev = dev;
 exports.imagenes = imagenes;
 exports.default = series( imagenes, css, dev );
+exports.build = series( cssbuild );
